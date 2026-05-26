798fdac668a506cded95813e59f86cf0f6af09c6
Daniel Ricardo Velasquez Quitian <dvelasquez@onvacation.com>
2026-05-26 01:03:16 -0500

    2026-05-26 - se añade red compartida y se agregan líneas opcionales para pruebas al script run.sh

---
+++ scripts/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# descomentar las siguientes líneas para ejecutar el programa directamente al iniciar el contenedor (pruebas)
+# cd /projects/golang1.26-echo5-hello-world
+# go run main.go
+
+
+
